5679935ead2fa1cae69bf7861aabe54eb2fe6eeb
Shuma Yoshioka <s64.stdio@gmail.com>
2015-03-14 13:24:13 +0900

    Update gulpfile.ts

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,7 +1,6 @@
 declare var require;
 
 var gulp        = require('gulp');
-//var $           = require('gulp-load-plugins');
 var browserSync = require('browser-sync');
 var ts          = require('gulp-typescript');
 var sass        = require('gulp-sass');
