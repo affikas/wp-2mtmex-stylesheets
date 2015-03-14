fec08c9dbc45a14f3485c6e91117e1017e3fbd8a
Shuma Yoshioka <s64.stdio@gmail.com>
2015-03-14 14:43:48 +0900

    gulpfile.ts

--- gulpfile.ts
+++ gulpfile.ts
@@ -8,9 +8,36 @@ var sourcemaps  = require('gulp-sourcemaps');
 var traceur     = require('gulp-traceur');
 var filter      = require('gulp-filter');
 var bowerFiles  = require('main-bower-files');
+var jade        = require('gulp-jade');
+var rename      = require('gulp-rename');
+
+var paths = {
+  scripts_ts:  'src/assets/scripts/**/*.ts',
+  styles_sass: 'src/assets/styles/**/*.{sass, scss}',
+  bower:       bowerFiles(bowerOptions),
+  jade:        'src/**.jade'
+};
+
+var dest_paths = {
+  scripts: 'dist/scripts',
+  styles:  'dist/styles',
+  bower:   'dist/bower_components',
+  jade:    'dist'
+}
+
+var filter_paths = {
+  scripts:   'scripts/**/*.js',
+  styles:    'styles/**/*.css',
+  bower:     'bower_components/**/*',
+  jade_php:  '**/*.php',
+  jade_html: '**/*.html'
+}
 
 var sassOptions  = {};
 var bowerOptions = {};
+var jadeOptions  = {
+  pretty: true
+};
 
 var tsProject = ts.createProject({
   target: 'ES6',
@@ -25,24 +52,6 @@ var tsProject = ts.createProject({
 var traceurOptions = {
   modules: 'commonjs'
 };
-
-var paths = {
-  scripts_ts:  'src/assets/scripts/**/*.ts',
-  styles_sass: 'src/assets/styles/**/*.{sass, scss}',
-  bower:       bowerFiles(bowerOptions)
-};
-
-var dest_paths = {
-  scripts: 'dist/scripts',
-  styles:  'dist/styles',
-  bower:   'dist/bower_components'
-}
-
-var filter_paths = {
-  scripts: 'dist/scripts/**/*.js',
-  styles:  'dist/styles/**/*.css',
-  bower:   'dist/bower_components/**/*'
-}
 
 gulp.task('styles', () => {
   return gulp.src([
@@ -74,11 +83,27 @@ gulp.task('scripts', () => {
 });
 
 gulp.task('bower', () => {
-  return gulp.src([
-    paths.bower
-  ])
+  return gulp.src(paths.bower)
     .pipe(gulp.dest(dest_paths.bower))
     .pipe(filter(filter_paths.bower))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('jade', () => {
+  return gulp.src(paths.jade)
+    .pipe(sourcemaps.init())
+    .pipe(jade(jadeOptions))
+    .pipe(sourcemaps.write())
+    .pipe(rename({
+      extname: ''
+    }))
+    .pipe(gulp.dest(dest_paths.jade))
+    .pipe(filter([
+      filter_paths.jade_php,
+      filter_paths.jade_html
+    ]))
     .pipe(browserSync.reload({
       stream: true
     }));
@@ -100,8 +125,12 @@ gulp.task('serve', ['default'], () => {
   gulp.watch([
     paths.bower
   ], ['bower']);
+
+  gulp.watch([
+    paths.jade
+  ], ['jade']);
 });
 
 gulp.task('default', [
-  'bower', 'scripts', 'styles'
+  'bower', 'scripts', 'styles', 'jade'
 ]);
